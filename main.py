b1b57172d664a57f06e4b7755524e11e153b9b4b
j-rein26 <grow435@gmail.com>
2024-10-29 13:17:34 -0400

    added title and description for api in main.py

--- main.py
+++ main.py
@@ -8,7 +8,18 @@ import models
 #create the database
 models.Base.metadata.create_all(bind=engine)
 
-app = FastAPI()
+app = FastAPI(
+    title = "Turkish Language API",
+    description = """
+    This is an api based on the book Lexicarry. The api allows  
+    the user to search 7 different categories (e.g. Functions, Sequences, etc.) 
+    and return the data for the specific category searched. 
+    The search is made in English and returns a dictionary of the 
+    category searched.
+
+    """
+
+)
 
 #Router from endpoints.py
 app.include_router(endpoints.router)
