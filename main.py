f95939ddf310b2b9446bdc9447e7a648bd6986de
j-rein26 <grow435@gmail.com>
2024-10-30 15:52:45 -0400

    added a frontend using fastapi templating

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Depends
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 from database import Base, engine
+from sqlalchemy.orm import Session
+from typing import List
 import endpoints
+from endpoints import get_db
 import models
+import json
 
 
 
@@ -21,8 +28,32 @@ app = FastAPI(
 
 )
 
+#connecting FastApi with static folder
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+#connecting FastApi with templates folder
+templates = Jinja2Templates(directory="templates")
+
 #Router from endpoints.py
 app.include_router(endpoints.router)
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request, db: Session = Depends(get_db)):
+    functions = db.query(models.Functions).all()  # Fetch all functions from the database
+    functions_json = json.dumps([  # Convert functions to JSON-like format
+        {
+            "id": func.id,
+            "english": func.english,
+            "turkish": func.turkish,
+            "examples": [example.strip() for example in func.examples.split(",")] if func.examples else []  # Split by comma and strip whitespace
+        }
+        for func in functions
+    ], ensure_ascii=False, indent=4)
+    return templates.TemplateResponse("index.html", {"request": request, "functions": functions, "functions_json": functions_json})
+
+
+
+
 
 
 
